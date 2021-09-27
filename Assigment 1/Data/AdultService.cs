56e906f5357ecba9a71cf902cd91aa0abee1fe13
Hansatang <portokarlos@gmail.com>
2021-09-27 10:40:51 +0200

    Initial commit

---
+++ Assigment 1/Data/AdultService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Models;
+
+namespace Assigment_1.Data
+{
+    public class AdultService : IAdultService
+    {
+        public IList<Adult> AdultsList { get; private set; }
+        private string productsFile = "adults.json";
+
+        public AdultService()
+        {
+            if (!File.Exists(productsFile))
+            {
+                string productsAsJson = JsonSerializer.Serialize(AdultsList);
+                File.WriteAllText(productsFile, productsAsJson);
+            }
+            else
+            {
+                string content = File.ReadAllText(productsFile);
+                AdultsList = JsonSerializer.Deserialize<List<Adult>>(content);
+            }
+        }
+
+
+        public void Save()
+        {
+            string productsAsJson = JsonSerializer.Serialize(AdultsList);
+            File.WriteAllText(productsFile, productsAsJson);
+        }
+    }
+}
---
+++ Assigment 1/Data/IAdultService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace Assigment_1.Data
+{
+    public interface IAdultService
+    {
+        public IList<Adult> AdultsList { get; }
+
+        public void Save();
+    }
+}
