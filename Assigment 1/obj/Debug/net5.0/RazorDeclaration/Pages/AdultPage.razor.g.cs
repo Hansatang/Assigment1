cceca9af204e4b57ab20e5f793ce72e08be57653
Hansatang <portokarlos@gmail.com>
2021-10-05 13:32:34 +0200

    23231

--- Assigment 1/obj/Debug/net5.0/RazorDeclaration/Pages/AdultPage.razor.g.cs
+++ Assigment 1/obj/Debug/net5.0/RazorDeclaration/Pages/AdultPage.razor.g.cs
@@ -13,84 +13,84 @@ namespace Assigment_1.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 1 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 2 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 3 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 4 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 5 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 6 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 7 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 8 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 9 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Assigment_1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 10 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Assigment_1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\AdultPage.razor"
+#line 5 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\AdultPage.razor"
 using Assigment_1.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\AdultPage.razor"
+#line 6 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\AdultPage.razor"
 using Models;
 
 #line default
@@ -106,7 +106,7 @@ using Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 49 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\AdultPage.razor"
+#line 49 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\AdultPage.razor"
        
 
 
--- Assigment 1/obj/Debug/net5.0/RazorDeclaration/Pages/Register.razor.g.cs
+++ Assigment 1/obj/Debug/net5.0/RazorDeclaration/Pages/Register.razor.g.cs
@@ -12,98 +12,98 @@ namespace Assigment_1.Pages
     using System.Linq;
     using System.Threading.Tasks;
 #nullable restore
-#line 1 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 1 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 2 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 3 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 4 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 5 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 6 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 7 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 8 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 9 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Assigment_1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\_Imports.razor"
+#line 10 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\_Imports.razor"
 using Assigment_1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\Register.razor"
+#line 2 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\Register.razor"
 using Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\Register.razor"
+#line 3 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\Register.razor"
 using Microsoft.AspNetCore.Components;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\Register.razor"
+#line 4 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\Register.razor"
 using Assigment_1.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\Register.razor"
+#line 5 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\Register.razor"
 using System.Collections;
 
 #line default
@@ -118,7 +118,7 @@ using System.Collections;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 35 "C:\Users\esben\RiderProjects\Assigment1\Assigment 1\Pages\Register.razor"
+#line 35 "C:\Users\krzys\RiderProjects\Assigment 1\Assigment 1\Pages\Register.razor"
        
 
     public ArrayList SecurityLevels = new()
